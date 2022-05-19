91d2ef1e66c3d72ca8f49700ba9b30faf2fec152
rmcouto <ricardo.couto-ext@viavarejo.com.br>
2022-05-19 17:39:14 -0300

    Estrutura Inicial - Automação Mobile com Appium

---
+++ src/main/java/br/ce/wcaquino/appium/CalculadoraTest.java
@@ -0,0 +1,25 @@
+package br.ce.wcaquino.appium;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+
+public class CalculadoraTest {
+	
+	public static void main(String[] args) throws MalformedURLException {
+		
+		DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+		desiredCapabilities.setCapability("platformName", "Android");
+		desiredCapabilities.setCapability("deviceName", "emulator-5554");
+		desiredCapabilities.setCapability("automationName", "uiautomator2");
+		desiredCapabilities.setCapability("appPackage", "com.android.calculator2");
+		desiredCapabilities.setCapability("appActivity", "com.android.calculator2.Calculator");
+        
+		AndroidDriver<MobileElement> driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), desiredCapabilities);
+	}
+
+}
