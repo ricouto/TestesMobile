df960c2fb0edbba1c481947c0039cd94446a4bf2
rmcouto <ricardo.couto-ext@viavarejo.com.br>
2022-05-27 09:19:18 -0300

    Inclusao do bloco before e after para melhorar a leitura do @test

--- src/main/java/br/ce/wcaquino/appium/DesafioCadastroAula18.java
+++ src/main/java/br/ce/wcaquino/appium/DesafioCadastroAula18.java
@@ -6,36 +6,54 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import io.appium.java_client.MobileBy;
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.remote.MobileCapabilityType;
 
-
 public class DesafioCadastroAula18 {
-
-	@Test
-	public void preencherCadastroDesafioAula18() throws MalformedURLException, InterruptedException {
-		
-		String[] elementosValidacaoCTAppium = {"Nome","Console","Switch","Checkbox"};
-		String[] massaValidacaoCTAppium = {"Video Game!","ps4","Off","Marcado"};
-		ArrayList<String> validarDadosForm = new ArrayList<String>();
+	
+	public WebDriver driver;
+	public WebDriverWait wait;
+	
+	@Before
+	public void setUp() throws Exception {
 		
 		// 1 - DesiredCapabilities
 		DesiredCapabilities dsCapabilities = new DesiredCapabilities();
 		dsCapabilities.setCapability("platformName", "Android");
 		dsCapabilities.setCapability("deviceName", "emulator-5554");
 		dsCapabilities.setCapability("automationName", "uiautomator2");
-		dsCapabilities.setCapability(MobileCapabilityType.APP, System.getProperty("user.dir") + "\\evidencias\\appTreinamento\\CTAppium_2_0.apk");
+		dsCapabilities.setCapability(MobileCapabilityType.APP,System.getProperty("user.dir") + "\\evidencias\\appTreinamento\\CTAppium_2_0.apk");
 
-		AndroidDriver<MobileElement> driver = new AndroidDriver<MobileElement>(new URL("http://127.0.0.1:4723/wd/hub"), dsCapabilities);
+		driver = new AndroidDriver<MobileElement>(new URL("http://127.0.0.1:4723/wd/hub"), dsCapabilities);
 
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		wait = new WebDriverWait(driver, 10);
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		//Finalizar o driver
+		driver.quit();
+	}
+	
+	@Test
+	public void preencherCadastroDesafioAula18() throws MalformedURLException, InterruptedException {
+		
+		String[] elementosValidacaoCTAppium = {"Nome","Console","Switch","Checkbox"};
+		String[] massaValidacaoCTAppium = {"Video Game!","ps4","Off","Marcado"};
+		ArrayList<String> validarDadosForm = new ArrayList<String>();
 
 		//2 - Acessando as opções do APP CTAppium
 		
@@ -65,11 +83,18 @@ public class DesafioCadastroAula18 {
 		driver.findElement(MobileBy.AccessibilityId("save")).click();
 		
 		//5 - Validar dos dados após Salvar
-		Thread.sleep(1000);// melhorar este ponto aqui com until
-		List<MobileElement> elementosRetornadosFormAPK = driver.findElements(By.xpath("//android.widget.TextView[@text]"));
-		//int i = 0;
-		// System.out.println("Qtd Elementos: "+ elementosRetornadosForm.size());
+		//Thread.sleep(1000);// melhorar este ponto aqui com until
+		wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.xpath("//android.widget.TextView[@text]")));
 		
+		List<MobileElement> elementosRetornadosFormAPK = driver.findElements(By.xpath("//android.widget.TextView[@text]"));
+		
+		// System.out.println("Qtd Elementos: "+ elementosRetornadosFormAPK.size());
+		
+		//Fiz estes dois blocos onde tentei outras estrategias, mas a APP, está == "Nome: Curso Appim" tudo junto e não separado / bonitinho
+		//para pegar o valor do elemento e fazer a assertion.... ótimo desafio!
+		//talvez tenha forma mais direta para a solução, mas, até o momento, não vi explicação no video e estou buscando outro caminho tbem!
+		
+		//Coloco no arrayList o valor (dado inserido no form) do 'elemento'
 		for (String elementoForm : elementosValidacaoCTAppium) {
 			for (MobileElement elementoRetornadoApk : elementosRetornadosFormAPK) {
 				if (elementoRetornadoApk.getText().contains(elementoForm)) {
@@ -79,7 +104,7 @@ public class DesafioCadastroAula18 {
 			}
 		}
 		
-		//validar retornoAPK contra MassaCTAppium
+		//Valido os dados do retornoAPK (primeiro bloco 'for' acima) contra MassaCTAppium (arrayList declarado no começo da classe)
 		for (String massaFormInput : massaValidacaoCTAppium) {
 			for (int i = 0; i < validarDadosForm.size(); i++) {
 				if(massaFormInput.trim().equals(validarDadosForm.get(i).toString())) {
@@ -88,8 +113,5 @@ public class DesafioCadastroAula18 {
 				}
 			}
 		}
-		
-		//99 - Finalizar o Driver
-		driver.quit();
 	}
 }
