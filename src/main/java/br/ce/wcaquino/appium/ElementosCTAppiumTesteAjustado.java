fa1dbbfd37a563bbafdd4a0e2d5443815bff4d3a
rmcouto <ricardo.couto-ext@viavarejo.com.br>
2022-06-14 20:37:55 -0300

    Inclusao do DSL e DriverFactory

--- src/main/java/br/ce/wcaquino/appium/ElementosCTAppiumTesteAjustado.java
+++ src/main/java/br/ce/wcaquino/appium/ElementosCTAppiumTesteAjustado.java
@@ -1,77 +1,55 @@
 package br.ce.wcaquino.appium;
 
 import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.concurrent.TimeUnit;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.remote.DesiredCapabilities;
 
+import br.ce.wcaquino.appium.core.DSL;
+import br.ce.wcaquino.appium.core.DriverFactory;
 import io.appium.java_client.MobileBy;
 import io.appium.java_client.MobileElement;
-import io.appium.java_client.android.AndroidDriver;
-import io.appium.java_client.remote.MobileCapabilityType;
 
 public class ElementosCTAppiumTesteAjustado {
 	
-	public AndroidDriver<MobileElement> driver;
+	private DSL dsl = new DSL(); 
 	
 	@Before
 	public void inicializarAppium() throws MalformedURLException {
-		DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
-		desiredCapabilities.setCapability("platformName", "Android");
-		desiredCapabilities.setCapability("deviceName", "emulator-5554");
-		desiredCapabilities.setCapability("automationName", "uiautomator2");
-		desiredCapabilities.setCapability(MobileCapabilityType.APP, "C:\\Git_Automacao\\CursoAppium\\evidencias\\appTreinamento\\CTAppium_2_0.apk");
-        
-		driver = new AndroidDriver<MobileElement>(new URL("http://127.0.0.1:4723/wd/hub"), desiredCapabilities);
-		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-		
-		//entrar no Formulario XPath
-		driver.findElement(By.xpath("//android.widget.TextView[@text='Formulário']")).click();
+		// entrar no Formulario XPath
+		dsl.clicarPorTexto("Formulário");
+		//dsl.clicar(By.xpath("//android.widget.TextView[@text='Formulário']"));
 	}
 	
 	@After
 	public void tearDown() {
-		driver.quit();
+		DriverFactory.killDriver();
 	}
 	
 	@Test
 	public void devePreencherCampoTexto() throws MalformedURLException {
 		//passos
-		//entrar no Formulario
-		/*List<MobileElement> elementosEncontrados = driver.findElements(By.className("android.widget.TextView"));
-		for (MobileElement elemento: elementosEncontrados) {
-			System.out.println(elemento.getText());
-		}
+		//escrever "nome" 
+		dsl.escrever(MobileBy.AccessibilityId("nome"), "Ricardo Couto");
 		
-		elementosEncontrados.get(1).click();*/
-
-		//escrever "nome" e
-		MobileElement campoNome = driver.findElement(MobileBy.AccessibilityId("nome"));//.sendKeys("Ricardo Couto");
-		campoNome.sendKeys("Ricardo Couto");
-		
-		//verificar o nome
-		String text = campoNome.getText();
-		Assert.assertEquals("Ricardo Couto", text);
+		//verificar o nome inputado
+		Assert.assertEquals("Ricardo Couto", dsl.obterTexto(MobileBy.AccessibilityId("nome")));
 	}
 
 	@Test
 	public void deveIntegarirCombo() throws MalformedURLException {
 		//passos
 		//clicar no combo
-		driver.findElement(MobileBy.AccessibilityId("console")).click();
+		dsl.selecionarCombo(MobileBy.AccessibilityId("console"), "Nintendo Switch");
 		
-		//selecionar a opcao
-		driver.findElement(By.xpath("//android.widget.CheckedTextView[@text='Nintendo Switch']")).click();
-		
-		//verificar a opcao
-		String textCombo = driver.findElement(By.xpath("//android.widget.Spinner/android.widget.TextView")).getText();
-		System.out.println(textCombo);
+		//selecionar e verificar a opcao
+		String textCombo = dsl.obterTexto(By.xpath("//android.widget.Spinner/android.widget.TextView"));
+		//System.out.println(textCombo);
 		Assert.assertEquals("Nintendo Switch", textCombo);
 	}
 	
@@ -79,18 +57,83 @@ public class ElementosCTAppiumTesteAjustado {
 	public void deveIntegarirSwitchCheckBox() throws MalformedURLException {
 		//passos
 		//verificar o status do elemento
-		MobileElement elCheck = driver.findElement(By.className("android.widget.CheckBox"));
-		MobileElement elSwitch = driver.findElement(MobileBy.AccessibilityId("switch"));
-		
-		Assert.assertTrue(elCheck.getAttribute("checked").equals("false"));
-		Assert.assertTrue(elSwitch.getAttribute("checked").equals("true"));
+		Assert.assertFalse(dsl.isCheckMarcado(By.className("android.widget.CheckBox")));
+		Assert.assertTrue(dsl.isCheckMarcado(MobileBy.AccessibilityId("switch")));
 		
 		//clicar nos elementos
-		elCheck.click();
-		elSwitch.click();
+		dsl.clicar(By.className("android.widget.CheckBox"));
+		dsl.clicar(MobileBy.AccessibilityId("switch"));
 		
 		//verificar estados alterados
-		Assert.assertFalse(elCheck.getAttribute("checked").equals("false")); //poderia mudar somente de false -> true
-		Assert.assertFalse(elSwitch.getAttribute("checked").equals("true")); //poderia mudar somente de true -> false
+		Assert.assertTrue(dsl.isCheckMarcado(By.className("android.widget.CheckBox")));
+		Assert.assertFalse(dsl.isCheckMarcado(MobileBy.AccessibilityId("switch")));
+	}
+	
+	@Test
+	public void preencherCadastro() throws MalformedURLException, InterruptedException {
+
+		String[] elementosValidacaoCTAppium = { "Nome", "Console", "Switch", "Checkbox" };
+		String[] massaValidacaoCTAppium = { "Video Game!", "ps4", "Off", "Marcado" };
+		ArrayList<String> validarDadosForm = new ArrayList<String>();
+
+		// 2 - Acessando as opções do APP CTAppium
+		// 2.2 - Preenchendo o "Formulário"
+		dsl.escrever(MobileBy.AccessibilityId("nome"), "Video Game!");
+
+		dsl.clicar(By.className("android.widget.Spinner"));
+		
+		dsl.clicar(By.xpath("//android.widget.CheckedTextView[@text='PS4']"));
+		
+		Assert.assertFalse(dsl.isCheckMarcado(MobileBy.AccessibilityId("check")));
+		Assert.assertTrue(dsl.isCheckMarcado(MobileBy.AccessibilityId("switch")));
+
+		dsl.clicar(MobileBy.AccessibilityId("check"));
+		dsl.clicar(MobileBy.AccessibilityId("switch"));
+
+		// 3 - Verificando os campos
+		Assert.assertEquals(dsl.obterTexto(MobileBy.AccessibilityId("nome")), "Video Game!");
+		Assert.assertEquals(dsl.obterTexto(By.xpath("//android.widget.Spinner/android.widget.TextView")),"PS4");
+		
+		Assert.assertTrue(dsl.isCheckMarcado(MobileBy.AccessibilityId("check")));
+		Assert.assertFalse(dsl.isCheckMarcado(MobileBy.AccessibilityId("switch")));
+
+		// 4 - Clicando no Salvar
+		dsl.clicar(MobileBy.AccessibilityId("save"));
+
+		// 5 - Validar dos dados após Salvar
+		Thread.sleep(2000);// melhorar este ponto aqui com until
+		//wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.xpath("//android.widget.TextView[@text]")));
+
+		List<MobileElement> elementosRetornadosFormAPK = dsl.obterLista(By.xpath("//android.widget.TextView[@text]"));
+
+		// System.out.println("Qtd Elementos: "+ elementosRetornadosFormAPK.size());
+
+		// Fiz estes dois blocos onde tentei outras estrategias, mas a APP, está ==
+		// "Nome: Curso Appim" tudo junto e não separado / bonitinho
+		// para pegar o valor do elemento e fazer a assertion.... ótimo desafio!
+		// talvez tenha forma mais direta para a solução, mas, até o momento, não vi
+		// explicação no video e estou buscando outro caminho tbem!
+
+		// Coloco no arrayList o valor (dado inserido no form) do 'elemento'
+		for (String elementoForm : elementosValidacaoCTAppium) {
+			for (MobileElement elementoRetornadoApk : elementosRetornadosFormAPK) {
+				if (elementoRetornadoApk.getText().contains(elementoForm)) {
+					// System.out.println("Deu certo... "+
+					// elementoRetornadoApk.getText().substring(elementoForm.length() + 2).trim());
+					validarDadosForm.add(elementoRetornadoApk.getText().substring(elementoForm.length() + 2).trim());
+				}
+			}
+		}
+
+		// Valido os dados do retornoAPK (primeiro bloco 'for' acima) contra
+		// MassaCTAppium (arrayList declarado no começo da classe)
+		for (String massaFormInput : massaValidacaoCTAppium) {
+			for (int i = 0; i < validarDadosForm.size(); i++) {
+				if (massaFormInput.trim().equals(validarDadosForm.get(i).toString())) {
+					System.out.println("deu certo né ... " + validarDadosForm.get(i).toString());
+					Assert.assertEquals(massaFormInput.trim(), validarDadosForm.get(i).toString());
+				}
+			}
+		}
 	}
 }
---
+++ src/main/java/br/ce/wcaquino/appium/core/DSL.java
@@ -0,0 +1,43 @@
+package br.ce.wcaquino.appium.core;
+
+import static br.ce.wcaquino.appium.core.DriverFactory.getDriver;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+
+import io.appium.java_client.MobileElement;
+
+public class DSL {
+
+	public void escrever(By by, String texto) {
+		getDriver().findElement(by).sendKeys(texto);
+	}
+
+	public String obterTexto(By by) {
+		return getDriver().findElement(by).getText();
+	}
+	
+	public List<MobileElement> obterLista(By by) {
+		return getDriver().findElements(by);
+	}
+
+	public void clicar(By by) {
+		getDriver().findElement(by).click();
+	}
+	
+	public void clicarPorTexto(String texto) {
+		clicar(By.xpath("//*[@text='"+texto+"']"));
+		//getDriver().findElement(By.xpath("//*[@text='"+texto+"']")).click();
+	}
+	
+	public void selecionarCombo(By by, String valor) {
+		getDriver().findElement(by).click();
+		clicarPorTexto(valor);
+		//getDriver().findElement(By.xpath("//android.widget.CheckedTextView[@text='"+valor+"']")).click();
+	}
+	
+	public boolean isCheckMarcado(By by) {
+		return getDriver().findElement(by).getAttribute("checked").equals("true");
+	}
+}
