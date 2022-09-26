c4c6feb819fa5a0bbabf138ee138c2b968b1c15c
rmcouto <ricardo.couto-ext@viavarejo.com.br>
2022-09-26 10:27:02 -0300

    Aula 49 - SauceLabs

--- src/main/java/br/ce/wcaquino/appium/core/DriverFactory.java
+++ src/main/java/br/ce/wcaquino/appium/core/DriverFactory.java
@@ -3,6 +3,8 @@ package br.ce.wcaquino.appium.core;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.MutableCapabilities;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
@@ -15,6 +17,7 @@ public class DriverFactory {
 	public static AndroidDriver<MobileElement> getDriver() {
 		if(driver == null) {
 			createDriver();
+			//createSauceLabsDriver();
 		}
 		return driver;
 	}
@@ -30,7 +33,27 @@ public class DriverFactory {
 		try {
 			driver = new AndroidDriver<MobileElement>(new URL("http://127.0.0.1:4723/wd/hub"), desiredCapabilities);
 		} catch (MalformedURLException e) {
-			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	}
+	
+	private static void createSauceLabsDriver() {
+		MutableCapabilities caps = new MutableCapabilities();
+		caps.setCapability("platformName","Android");
+		caps.setCapability("appium:deviceName","Android GoogleAPI Emulator");
+		caps.setCapability("appium:deviceOrientation", "portrait");
+		caps.setCapability("appium:platformVersion","7.0");
+		caps.setCapability("appium:automationName", "UiAutomator2");
+		caps.setCapability("appium:app", "storage:filename=CTAppium_1_2.apk");
+		MutableCapabilities sauceOptions = new MutableCapabilities();
+		sauceOptions.setCapability("build", "oauth-qualidadevia2022-44603");
+		sauceOptions.setCapability("name", "c31abe53-f2e8-415c-b21b-3aee75319a07");
+		caps.setCapability("sauce:options", sauceOptions);
+
+		try {
+			driver = new AndroidDriver<MobileElement>(new URL("https://oauth-qualidadevia2022-44603:c31abe53-f2e8-415c-b21b-3aee75319a07@ondemand.us-west-1.saucelabs.com:443/wd/hub"), caps);
+		} catch (MalformedURLException e) {
 			e.printStackTrace();
 		}
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
